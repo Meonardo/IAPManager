42109915b70004feab912427ff5f57cdd71281a0
Meoanrdo <sisxme@gmail.com>
2017-08-21 18:06:14 +0800

    Initial Commit

---
+++ IAPManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IAPManager
+//
+//  Created by Meonardo on 2017/8/21.
+//  Copyright © 2017年 Meonardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IAPManager/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IAPManager
+//
+//  Created by Meonardo on 2017/8/21.
+//  Copyright © 2017年 Meonardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
